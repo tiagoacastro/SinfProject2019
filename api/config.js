8873a9e433a94683663d3e6cfc511ebb86192d22
Tiago <tiagoaraujocastro@gmail.com>
2019-12-11 17:15:02 +0000

    big fixes

--- api/config.js
+++ api/config.js
@@ -3,11 +3,13 @@ require('dotenv').config()
 const { Pool } = require('pg')
 const isProduction = process.env.NODE_ENV === 'production'
 
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
-
 const pool = new Pool({
-  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  ssl: isProduction,
-})
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT,
+    host: process.env.DB_HOST,
+    ssl: true,
+});
 
 module.exports = { pool }
