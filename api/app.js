f77788bc29a57c6c70d98d907f680dc7d2a23d70
Tiago <tiagoaraujocastro@gmail.com>
2019-12-16 19:20:59 +0000

    sorry bea

--- api/app.js
+++ api/app.js
@@ -35,17 +35,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/products', productsRouter);
 app.use('/entities', entitiesRouter);
-//app.use('/company/:companyID/sales', salesRouter);
+app.use('/company/:companyID/sales', salesRouter);
 app.use('/company/:companyID/purchases', purchasesRouter);
 app.use('/processes', processesRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
     next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -105,7 +105,7 @@ async function initialize() {
 //testDB();
 
 initialize().then(
-    async () => {
+    async() => {
         await getPurchaseOrders(companies[0], companies[1]);
         //await getDeliveryOrders(companies[0], companies[1]);
         //await getPayments(companies[0], companies[1]);
